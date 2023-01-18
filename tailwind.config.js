54c24dbbb552db25ce6c5043178b1b61b5ed2562
khazarglzd <khazarguluzade@gmail.com>
2023-01-19 01:12:28 +0400

    adding google fonts & colors to prj

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container:{
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+      colors: {
+        'movielio-red': '#BC1A45',
+        'movielio-melon': '#FFD369',
+        'movielio-grey': '#DDDDDD',
+        'movielio-white': '#F7F7F7',
+      },
+    },
   },
   plugins: [],
 }
